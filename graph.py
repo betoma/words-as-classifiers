a6777aa554da7b6462179162e33fa0b6d4b6be87
betoma <bethanyetoma@outlook.com>
2020-10-07 14:31:11 +0200

    tweak threshold graph

--- graph.py
+++ graph.py
@@ -13,9 +13,9 @@ with open("data/rand_results", "rb") as f:
 
 thresholds = list(np.linspace(0.7, 0.9, 20))
 
-fig, axes = plt.subplots(5, 2, figsize=(8, 15))
+fig, axes = plt.subplots(5, 2, figsize=(8, 13))
 plt.subplots_adjust(hspace=0.5)
-fig.suptitle("Performance across thresholds for different numbers of confusors")
+# fig.suptitle("Performance across thresholds for different numbers of confusors")
 
 for ax, model, random in zip(axes.flatten(), vv, rvv):
     a = [y[1] for y in model[1]]
@@ -24,5 +24,7 @@ for ax, model, random in zip(axes.flatten(), vv, rvv):
     ax.plot(thresholds, ra, lw=5, alpha=0.8, label="random")
     ax.set(title=f"{model[0]} Confusors", xlabel="Threshold value", ylabel="Accuracy")
 handles, labels = ax.get_legend_handles_labels()
-fig.legend(handles, labels, title="Prediction Method")
+fig.legend(
+    handles, labels, title="Prediction Method", loc="lower center",  # mode="expand",
+)
 plt.savefig("images/best_thresholds.png", bbox_inches="tight")
