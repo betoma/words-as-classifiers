f9cc30a03ef54964be097a780eedb9d405bb91bd
betoma <bethanyetoma@outlook.com>
2020-10-07 14:12:56 +0200

    change from jupyter notebook to standalone scripts

---
+++ graph.py
@@ -0,0 +1,28 @@
+import pickle
+from random import getrandbits
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+with open("data/naive_sem_results", "rb") as f:
+    vv = pickle.load(f)
+
+with open("data/rand_results", "rb") as f:
+    rvv = pickle.load(f)
+
+thresholds = list(np.linspace(0.7, 0.9, 20))
+
+fig, axes = plt.subplots(5, 2, figsize=(8, 15))
+plt.subplots_adjust(hspace=0.5)
+fig.suptitle("Performance across thresholds for different numbers of confusors")
+
+for ax, model, random in zip(axes.flatten(), vv, rvv):
+    a = [y[1] for y in model[1]]
+    ra = [y[1] for y in random[1]]
+    ax.plot(thresholds, a, lw=5, alpha=0.8, label="semantic")
+    ax.plot(thresholds, ra, lw=5, alpha=0.8, label="random")
+    ax.set(title=f"{model[0]} Confusors", xlabel="Threshold value", ylabel="Accuracy")
+handles, labels = ax.get_legend_handles_labels()
+fig.legend(handles, labels, title="Prediction Method")
+plt.savefig("images/best_thresholds.png", bbox_inches="tight")
---
+++ naive_sim.py
@@ -0,0 +1,94 @@
+from collections import Counter
+import pickle
+from random import getrandbits
+
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.stats import beta, uniform
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
+from sklearn.model_selection import RandomizedSearchCV
+import stanza
+import torch
+from tqdm import tqdm, trange
+
+from utils.depparse import DepParse
+from utils.fakedata import FakeWorld, FakeData
+from utils.semparse import ClassicalSem
+
+torch.cuda.empty_cache()
+
+fig, ax = plt.subplots(1, 1)
+a, b = 5, 1.3
+a2, b2 = 5, 1.4
+a3, b3 = 5, 1.5
+a4, b4 = 5, 2.2
+a5, b5 = 4, 5
+
+x = np.linspace(beta.ppf(0.01, 1, 1), beta.ppf(0.99, 1, 1), 100)
+ax.plot(x, beta.pdf(x, a, b), "k-", lw=5, alpha=0.8)
+ax.plot(x, beta.pdf(x, a2, b2), "g-", lw=5, alpha=0.6)
+ax.plot(x, beta.pdf(x, a3, b3), "b-", lw=5, alpha=0.6)
+ax.plot(x, beta.pdf(x, a4, b4), "y-", lw=5, alpha=0.6)
+ax.plot(x, beta.pdf(x, a5, b5), "r-", lw=5, alpha=0.6)
+plt.savefig("images/classifier_distributions.png", bbox_inches="tight")
+
+distributions = {
+    "exact": beta(a, b),
+    "super": beta(a2, b2),
+    "close": beta(a3, b3),
+    "ish": beta(a4, b4),
+    "not": beta(a5, b5),
+}
+
+p = stanza.Pipeline(
+    lang="en",
+    processors={
+        "tokenize": "spacy",
+        "mwt": "default",
+        "pos": "default",
+        "lemma": "default",
+        "depparse": "default",
+    },
+)
+
+thresholds = list(np.linspace(0.7, 0.9, 20))
+examples_per_dataset = 150
+values = []
+random_values = []
+for n in trange(1, 11):
+    vals = []
+    rand_vals = []
+    w = FakeWorld(n, n, dists=distributions)
+    s = ClassicalSem(parser=p)
+    for t in tqdm(thresholds):
+        acc_scores = []
+        rand_acc_scores = []
+        f1_scores = []
+        rand_f1_scores = []
+        s.set_threshold(t)
+        for _ in range(5):
+            d = FakeData(w, examples_per_dataset)
+            results = s.score(d)
+            acc = results[0]
+            f1 = results[1]
+            acc_scores.append(acc)
+            f1_scores.append(f1)
+            rand_r = [getrandbits(1) for _ in range(examples_per_dataset)]
+            rand_acc, rand_f1 = s.score(d, results=rand_r)
+            rand_acc_scores.append(rand_acc)
+            rand_f1_scores.append(rand_f1)
+        vals.append((t, acc_scores, f1_scores))
+        rand_vals.append((t, rand_acc_scores, rand_f1_scores))
+    values.append((n, vals))
+    random_values.append((n, rand_vals))
+
+vv = [(x[0], [(y[0], np.mean(y[1]), np.mean(y[2])) for y in x[1]]) for x in values]
+rvv = [
+    (x[0], [(y[0], np.mean(y[1]), np.mean(y[2])) for y in x[1]]) for x in random_values
+]
+
+with open("data/naive_sem_results", "wb") as f:
+    pickle.dump(vv, f)
+
+with open("data/rand_results", "wb") as f:
+    pickle.dump(rvv, f)
